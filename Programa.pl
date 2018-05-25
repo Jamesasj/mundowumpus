7b1dc85af8d7810169bfdb37be20c8194fd58db9
James <james@jamesalves.com.br>
2018-05-25 20:50:42 -0300

    Adicionado atuadores do conhecimento.
    Adicionado atuadores do conhecimento para adicionar fatos a base de conhecimento.

--- Programa.pl
+++ Programa.pl
@@ -10,12 +10,14 @@
         eh_burado/2,
         eh_wumpus/2,
         eh_ouro/2]).
-
+pos_agente([1,1]).
 principal:-
     inicializar,
-    caminhar([1,1]).
+    caminhar().
 
 inicializar:- 
+    assert(dimensao([4,4])),
+    assert(pos_ouro([3,2])),
     assert(pos_wumpus([4,1])),
     assert(pos_buraco([4,4])),
     assert(pos_buraco([3,3])),
@@ -23,12 +25,45 @@ inicializar:-
     assert(pos_agente([1,1])).
     
 
-caminhar([X,Y]):-
+caminhar():-
     obter_percepcao(Percepcao),
     atualizar_conhecimento(Percepcao).
 
 /*      Base de conhecimento        */
+atualizar_conhecimento([Fedor, Brisa, Brilho]):-
+    add_wampus_kb(Fedor),
+    add_brisa_kb(Brisa),
+    add_ouro_kb(Brilho).  
 
+add_wampus_kb(nao):- 
+    pos_agente([X,Y]),
+    Y1 is Y+1, gravar_wumpus(nao, [X,Y1]),
+    Y2 is Y-1, gravar_wumpus(nao, [X,Y2]),
+    X1 is X+1, gravar_wumpus(nao, [X1,Y]),
+    X2 is X-1, gravar_wumpus(nao, [X2,Y]).
+
+add_brisa_kb(nao):- 
+    pos_agente([X,Y]),
+    Y1 is Y+1, gravar_brisa(nao, [X,Y1]),
+    Y2 is Y-1, gravar_brisa(nao, [X,Y2]),
+    X1 is X+1, gravar_brisa(nao, [X1,Y]),
+    X2 is X-1, gravar_brisa(nao, [X2,Y]).
+
+add_ouro_kb(nao):- 
+    pos_agente([X,Y]),
+    gravar_ouro(nao, [X,Y]).
+
+gravar_wumpus(nao, Pos):-
+    retractall(eh_wumpus(_, Pos)),
+    assert(eh_wumpus(nao, Pos)).
+
+gravar_brisa(nao, Pos):-
+    retractall(eh_burado(_, Pos)),
+    assert(eh_burado(nao, Pos)).
+
+gravar_ouro(nao, Pos):-
+    retractall( eh_ouro(_, Pos)),
+    assert(eh_ouro(nao, Pos)).
 
 /*      Sensores        */
 obter_percepcao([Fedor, Brisa, Brilho]):-
@@ -51,8 +86,7 @@ sentir(nao).
 enchergar(sim):-
     pos_agente(Pa),
     pos_ouro(Pa).
-
-enchegar(nao).
+enchergar(nao).
 
 /*      Util        */
 adj([Xa,Ya],[Xb,Yb]):-
@@ -64,23 +98,13 @@ adj([Xa,Ya],[Xb,Yb]):-
 
 
 validaPosicao(X,Y):-
-    dimensao(Px,Py),
+    dimensao([Px,Py]),
     X > 0, X =< Px,
     Y > 0, Y =< Py.
 
 
 
 /*      Atuadores       */
-virarDireita:- print("virar a direita").
-virarEsquerda:- print("virar a direita").
-paraFrente:- print("virar para frente").
-agarrar:- print("agarrar").
-atirar:- print("atirar").
-escalar:- print("escalar").
-
-
-impacto(X,Y):- print("impacto").
-grito(X,Y):- print("grito").
 
 /**
 escolher():-
