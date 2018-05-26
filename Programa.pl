e43a38bc4be5dde67e2b0ae2170f4a316920a9d4
James <james@jamesalves.com.br>
2018-05-25 21:39:09 -0300

    Inicio da caminhada do agente
    Inicio da caminhada do agente

--- Programa.pl
+++ Programa.pl
@@ -10,7 +10,7 @@
         eh_burado/2,
         eh_wumpus/2,
         eh_ouro/2]).
-pos_agente([1,1]).
+
 principal:-
     inicializar,
     caminhar().
@@ -27,9 +27,17 @@ inicializar:-
 
 caminhar():-
     obter_percepcao(Percepcao),
-    atualizar_conhecimento(Percepcao).
+    atualizar_conhecimento(Percepcao),
+    perguntar(Acao).
 
-/*      Base de conhecimento        */
+/*      Base de c1onhecimento        */
+perguntar(Acao):-
+    eh_wumpus(nao, L),
+    eh_burado(nao, L),
+    autualizar_pos(L),
+    permitido(L),
+    Acao = L.
+
 atualizar_conhecimento([Fedor, Brisa, Brilho]):-
     add_wampus_kb(Fedor),
     add_brisa_kb(Brisa),
@@ -53,6 +61,10 @@ add_ouro_kb(nao):-
     pos_agente([X,Y]),
     gravar_ouro(nao, [X,Y]).
 
+add_ouro_kb(sim):- 
+    pos_agente([X,Y]),
+    gravar_ouro(sim, [X,Y]).
+
 gravar_wumpus(nao, Pos):-
     retractall(eh_wumpus(_, Pos)),
     assert(eh_wumpus(nao, Pos)).
@@ -64,6 +76,14 @@ gravar_brisa(nao, Pos):-
 gravar_ouro(nao, Pos):-
     retractall( eh_ouro(_, Pos)),
     assert(eh_ouro(nao, Pos)).
+
+gravar_ouro(sim, Pos):-
+    retractall( eh_ouro(_, Pos)),
+    assert(eh_ouro(sim, Pos)).
+
+autualizar_pos(Pos):-
+    retractall(pos_agente(Pos)),
+    assert(pos_agente(Pos)).
 
 /*      Sensores        */
 obter_percepcao([Fedor, Brisa, Brilho]):-
@@ -95,14 +115,15 @@ adj([Xa,Ya],[Xb,Yb]):-
     Xa = Xb-1, Ya = Yb+1,
     Xa = Xb-1, Ya = Yb-1.
 
-
-
 validaPosicao(X,Y):-
     dimensao([Px,Py]),
     X > 0, X =< Px,
     Y > 0, Y =< Py.
 
-
+permitido([X,Y]):-
+    dimensao([Dx,Dy]),
+    0<X,X>Dx+1,
+    0<Y,Y<Dy+1.
 
 /*      Atuadores       */
 
